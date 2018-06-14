035ee4ace844c25d2b94df6206a1ac75248bdfb0
Nyasha Nyaguwa <nyashan@cmahq.co.za>
2018-06-14 17:00:13 +0200

    set up components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Welcome from './components/Welcome';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-      TEST
+      <div>        
+        <Welcome />
       </div>
     );
   }
---
+++ src/components/Welcome.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+    render() {
+        return (
+            <div>
+                <img src="Batman" alt="" />
+                <h2>Welcome to</h2>
+                <h1>Gotham</h1>
+            </div>
+        );
+    }
+}
+
+export default Welcome;
